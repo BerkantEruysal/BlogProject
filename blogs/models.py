16a4e588d9aec7e60684684919de2634fb7c5942
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2022-07-13 13:50:21 +0300

    Change naming of models

--- blogs/models.py
+++ blogs/models.py
@@ -7,21 +7,31 @@ class Blog(models.Model):
     header = models.CharField(max_length=50)
     creation_time = models.DateTimeField(auto_now_add= True)
     content_text = models.TextField()
-    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.header
 
 class Comment(models.Model):
     comment_text = models.TextField()
-    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
-    blog_id = models.ForeignKey('Blog', on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment_text[:20] + "..."
 
 class BlogLike(models.Model):
-    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    blog_id = models.ForeignKey('Blog', on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE)
+
+    def __str__(self):
+        print(self.blog_id.header)
+        return "asd"
 
 class CommentLike(models.Model):
-    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    comment_id = models.ForeignKey('Comment', on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
 
 
         
