31f4c2d8786094ed820f39b3698ffd58fdce7d9e
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2022-07-28 18:24:13 +0300

    Add edit blog view, template and url

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from blogs.views import IndexView, BlogDetailView, SignUpView, CreateBlogView
+from blogs.views import IndexView, BlogDetailView, SignUpView, CreateBlogView, EditBlogView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('<int:pk>/', BlogDetailView.as_view(), name='blogDetail'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
-    path('createBlog/', CreateBlogView.as_view(), name='createBlog')
-
+    path('create-blog/', CreateBlogView.as_view(), name='create_blog'),
+    path('<int:pk>/edit', EditBlogView.as_view(), name='edit_blog')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,8 +2,9 @@ from django.views.generic import ListView, DetailView
 from blogs.models import Blog, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
 class IndexView(ListView):
     model = Blog
@@ -13,7 +14,7 @@ class IndexView(ListView):
 
 class BlogDetailView(DetailView):
     model = Blog
-    template_name = 'blogs/blogDetail.html'
+    template_name = 'blogs/blog_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -30,9 +31,31 @@ class SignUpView(CreateView):
 class CreateBlogView(LoginRequiredMixin, CreateView):
     model = Blog
     success_url = reverse_lazy('index')
-    template_name = 'blogs/createBlog.html'
+    template_name = 'blogs/create_blog.html'
     fields = ['header', 'content_text']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class EditBlogView(LoginRequiredMixin, UpdateView ):
+    model = Blog
+    fields = ['header', 'content_text']
+    template_name = "blogs/edit_blog.html"
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy("blog_detail", kwargs={'pk': self.object.pk})
+
+    def user_passes_test(self, request):
+        if request.user.is_authenticated:
+            self.object = self.get_object()
+            return self.object.user == request.user
+        return False
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.user_passes_test(request):
+            raise PermissionDenied
+        return super(EditBlogView, self).dispatch(request, *args, **kwargs)
+
+
+
